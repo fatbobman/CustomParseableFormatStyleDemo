f4ef64a3429f34edbbdc180162a2f7f3526a43c7
Yang Xu <xuyang@me.com>
2021-09-29 20:20:15 +0800

    Initial Commit

---
+++ CustomParseableFormatStyleDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CustomParseableFormatStyleDemo
+//
+//  Created by Yang Xu on 2021/9/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CustomParseableFormatStyleDemo/CustomParseableFormatStyleDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomParseableFormatStyleDemoApp.swift
+//  CustomParseableFormatStyleDemo
+//
+//  Created by Yang Xu on 2021/9/29.
+//
+
+import SwiftUI
+
+@main
+struct CustomParseableFormatStyleDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
